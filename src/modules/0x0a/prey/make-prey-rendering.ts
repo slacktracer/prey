0720736f1a5bc75bbd2964760aff7c6e501a437c
slacktracer <slacktracer@gmail.com>
2025-08-27 21:42:34 -0300

    Putting the fin (simplified) back.

--- src/modules/0x0a/prey/make-prey-rendering.ts
+++ src/modules/0x0a/prey/make-prey-rendering.ts
@@ -26,6 +26,26 @@ export const makePreyRendering: MakePreyRendering = ({ renderingSettings }) => {
 
   rendering.add(preyBody);
 
+  const finGeometry = new BoxGeometry(
+    renderingSettings.height * 0.15,
+    renderingSettings.height * 0.3,
+    renderingSettings.depth * 0.2,
+  );
+
+  const finMaterial = new MeshPhongMaterial({
+    color: 0x2266ff,
+
+    flatShading: true,
+  });
+
+  const fin = new Mesh(finGeometry, finMaterial);
+
+  fin.position.set(renderingSettings.depth * 0.1, renderingSettings.height, 0);
+
+  fin.rotation.z = Math.PI / 2.25;
+
+  rendering.add(fin);
+
   const candle = new PointLight(renderingSettings.candle.color, 40, 8);
 
   candle.position.set(
