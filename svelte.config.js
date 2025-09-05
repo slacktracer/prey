e43fba2ed2a7ad66f720147957997fd604dbdf50
slacktracer <slacktracer@gmail.com>
2025-09-05 16:43:47 -0300

    Using the node adapter to be able to deploy to render. Trying to anyway.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
+import process from "node:process";
+
 import adapter from "@deno/svelte-adapter";
+import nodeAdapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+console.info("found", process.env.SVELTE_ADAPTER);
+
+const useNodeAdapter = process.env.SVELTE_ADAPTER === "node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: useNodeAdapter ? nodeAdapter() : adapter(),
   },
 
   preprocess: vitePreprocess(),
